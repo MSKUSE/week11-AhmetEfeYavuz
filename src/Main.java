5fb326cb3a1972f99ae1c050c60a223c4b614b13
AhmetEfeYavuz <ahmetefeyavuz@posta.mu.edu.tr>
2025-05-23 11:52:16 +0300

    Java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyEmptyStackException{
 
         Stack stack = new StackLinkedList();
-        stack.push("This is a string");
-        stack.push(5);
-        stack.peek();
-        System.out.println("Is empty "
-        +stack.isEmpty() );
-        System.out.println("Pop = " +
-                stack.pop());
-        stack.peek();
+        try {
+            stack.pop();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+
     }
 }
